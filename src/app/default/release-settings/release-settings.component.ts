7bf9f198b36d418eeeb8266e266fface71a11ee0
Tushar <tusharpatil2912@gmail.com>
2021-07-06 16:24:37 +0530

    release settings added

--- src/app/default/release-settings/release-settings.component.ts
+++ src/app/default/release-settings/release-settings.component.ts
@@ -16,14 +16,18 @@ export class ReleaseSettingsComponent implements OnInit {
 
   columnDefs = [
     { headerName:'ID',field: 'id', maxWidth: 80,minWidth: 80, resizable: true, sortable: true, filter: true },
-    { headerName:'Project Name',field: 'name', width: 650,minWidth: 80, resizable: true, sortable: true, filter: true },
-    { headerName:'Tasks',field: 'phase', width: 150,minWidth: 135, resizable: true, sortable: true, filter: true },
-    { headerName:'Open Tasks',field: 'id', width: 150,minWidth: 135, resizable: true, sortable: true, filter: true},
-    { headerName:'Details',field: 'id', width: 150,minWidth: 145, resizable: true, sortable: true, filter: true},
-    { headerName:'Settings',field: 'id', width: 150,minWidth: 80, resizable: true, sortable: true,  filter: true}
+    { headerName:'Project Name',field: 'name', width: 350,minWidth: 80, resizable: true, sortable: true, filter: true },
+    { headerName:'Tasks',field: 'tasks', width: 150,minWidth: 135, resizable: true, sortable: true, filter: true },
+    { headerName:'Open Tasks',field: 'opentasks', width: 150,minWidth: 135, resizable: true, sortable: true, filter: true},
+    { headerName:'Details',field: 'details', width: 250,minWidth: 145, resizable: true, sortable: true, filter: true},
+    { headerName:'Select',field: 'select', width: 80,minWidth: 80, resizable: true, sortable: true,  filter: true, checkboxSelection: true}
 ];
 
-rowData = null;
+rowData = [
+  {id:'1',name:'Project 1',details:'ABC',tasks:'5',opentasks:'4',select:''},
+  {id:'2',name:'Project 2',details:'ABC',tasks:'8',opentasks:'2',select:''},
+  {id:'3',name:'Project 3',details:'ABC',tasks:'7',opentasks:'3',select:''}
+];
 
 onGridReady(params) {
   this.gridApi = params.api;
