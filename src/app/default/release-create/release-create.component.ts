e2d00423b145b85eb7dc657d6da934cce72a3a8d
Tushar <tusharpatil2912@gmail.com>
2021-08-04 20:00:31 +0530

    added release services

--- src/app/default/release-create/release-create.component.ts
+++ src/app/default/release-create/release-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectDetailsService } from "../../services/project-details.service";
+import { NotifierService } from "angular-notifier";
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-release-create',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReleaseCreateComponent implements OnInit {
 
-  constructor() { }
+  releaseDetailsForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private api: ProjectDetailsService,
+    private router: Router,
+    private notifier: NotifierService) { }
 
   ngOnInit(): void {
+    this.releaseDetailsForm = this.fb.group({
+      id: [''],
+      name: [''],
+      details:[''],
+      releaseDate: [''],
+      status:['']
+    });
+  }
+
+  onSubmit(){
+    const jsonForm ={
+      name:this.releaseDetailsForm.get('name').value,
+      details: this.releaseDetailsForm.get('details').value,
+      releaseDate: this.releaseDetailsForm.get('releaseDate').value,
+      status: this.releaseDetailsForm.get('status').value
+    };
+
+    this.api.addNewRelease(jsonForm).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+    this.router.navigate(['/release']).then(()=>{
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
+
+    this.notifier.notify("success", "New Release Added Successfully!");
   }
 
 }
