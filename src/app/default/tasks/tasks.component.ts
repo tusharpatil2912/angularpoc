08fc665506ed5e9d2a9d42389ae48049e7c9e886
i723884 <sunil.b@voya.com>
2021-08-13 20:03:30 +0530

    All task screen changes data binding from API

--- src/app/default/tasks/tasks.component.ts
+++ src/app/default/tasks/tasks.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskbuttonComponent } from '../agCustomCells/taskbutton/taskbutton.component';
+import { TaskDetailsService } from "../../services/task-details.service";
+import { NotifierService } from "angular-notifier";
 
 
 @Component({
@@ -9,26 +11,39 @@ import { TaskbuttonComponent } from '../agCustomCells/taskbutton/taskbutton.comp
 })
 export class TasksComponent implements OnInit {
   private gridApi;
+  taskList;
+  mockupprojectList=[{"id":1,"name":"My First Project","description":"Desc 1","owner":"Owner 1","sme":"Sme 1","phase":null,"codeDropDate":null,"codeFreezeDate":null,"releaseDate":null,"createdDate":"2020-11-26"},{"id":2,"name":"My Second Project","description":"Desc 2","owner":"Owner 2","sme":"Sme 2","phase":null,"codeDropDate":null,"codeFreezeDate":null,"releaseDate":null,"createdDate":"2020-11-27"},{"id":3,"name":"My Third Project","description":"Desc 3","owner":"Owner 3","sme":"Sme 3","phase":null,"codeDropDate":null,"codeFreezeDate":null,"releaseDate":null,"createdDate":"2020-12-01"},{"id":4,"name":"My Fourth Project","description":"Desc 4","owner":"Owner 4","sme":"Sme 4","phase":null,"codeDropDate":null,"codeFreezeDate":null,"releaseDate":null,"createdDate":"2020-12-01"}];
 
-  constructor() { }
+
+  constructor(private detailsapi: TaskDetailsService,private notifier: NotifierService,) { }
 
   ngOnInit(): void {
+    this.detailsapi.getTasksList().subscribe((data)=>{
+      //console.log(data);
+      this.taskList = data;
+      this.rowData = data;
+    },(error)=>{
+      this.rowData=this.mockupprojectList;
+      this.notifier.notify("error", "API Error. Showing Mockup Data");
+    });
   }
   columnDefs = [
-    { headerName:'Sl No',field: 'id', width: 100,minWidth: 100, resizable: true, sortable: true, filter: true },
-    { headerName:'Task Id',field: 'id', width: 100,minWidth: 100, resizable: true, sortable: true, filter: true },
-    { headerName:'Task description',field: 'name', width: 520,minWidth: 100, resizable: true, sortable: true, filter: true },
-    { headerName:'Created Date',field: 'id', width: 150,minWidth: 100, resizable: true, sortable: true, filter: true},
-    { headerName:'Assigned to',field: 'name', width: 150,minWidth: 100, resizable: true, sortable: true, filter: true},
-    { headerName:'Status',field: 'taskId', cellRendererFramework: TaskbuttonComponent, width: 150,minWidth: 80, resizable: true, sortable: true,  filter: true},
-    { headerName:'Reason',field: 'id', width: 150,minWidth: 100, resizable: true, sortable: true,  filter: true}
+    { headerName:'Task Id',field: 'taskId', width: 100,minWidth: 100, resizable: true, sortable: true, filter: true },
+    { headerName:'TaskName',field: 'taskName', width: 200,minWidth: 100, resizable: true, sortable: true,  filter: true},
+    { headerName:'Task description',field: 'taskDetails', width: 250,minWidth: 100, resizable: true, sortable: true, filter: true },
+    { headerName:'Created Date',field: 'taskCreatedDate', width: 150,minWidth: 100, resizable: true, sortable: true, filter: true},
+    { headerName:'Assigned to',field: 'taskOwner', width: 150,minWidth: 100, resizable: true, sortable: true, filter: true},
+    { headerName:'Status',field: 'taskId', cellRendererFramework: TaskbuttonComponent, width: 150,minWidth: 80, resizable: true, sortable: true,  filter: true}
+
+    
 ];
 
-rowData=[
-  {taskId:'1',id:'1',name:'rishi'},
-  {taskId:'2',id:'2',name:'tushar'},
-  {taskId:'3',id:'3',name:'chethan'}
-];
+rowData= null;
+//[
+ // {taskId:'1',id:'1',name:'rishi'},
+  //{taskId:'2',id:'2',name:'tushar'},
+  //{taskId:'3',id:'3',name:'chethan'}
+//];
 
 onGridReady(params) {
   this.gridApi = params.api;
